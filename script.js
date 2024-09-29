a87beca70e7190335829c4c189d6a48ebe7b48f8
slim abida <slimabida21@gmail.com>
2024-09-30 00:19:46 +0200

    ajout infos manquantes de balise body index.html

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
+// la partie prestations // 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".details-btn");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", (event) => {
+            const formule = event.target.getAttribute("data-formule");
+            if (formule === "quartet") {
+                alert("Formule Quartet : Maximum de groove pour 933 €");
+            } else if (formule === "duo") {
+                alert("Formule Duo : Plus intimiste pour 465 €");
+            }
+        });
+    });
+});
